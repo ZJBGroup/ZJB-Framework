ee760feb1db95c4261bb6a2bb3fd71f79ff2f196
wangchen <wangchen@zhejianglab.com>
2023-10-24 17:37:23 +0800

    build: 更新版本号

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name='zjb-framework',
-    version='0.2.0',
-    install_requires=[
-        'traits>=6.4.1',
-        'ulid-py'
-    ],
+    name="zjb-framework",
+    version="0.2.5",
+    install_requires=["traits>=6.4.1", "ulid-py"],
     packages=setuptools.find_packages(),
 )
