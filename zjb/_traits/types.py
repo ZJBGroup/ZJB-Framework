ada2ce812708efa155cf5edd95110b1c7e87456f
wangchen <wangchen@zhejianglab.com>
2023-10-24 11:30:45 +0800

    type: 增强traits类的静态类型提示

--- zjb/_traits/types.py
+++ zjb/_traits/types.py
@@ -1,13 +1,18 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Generic, ParamSpec, TypeVar
 
 from traits.trait_types import Any as TraitAny
 from traits.trait_types import Callable as _Callable
 from traits.trait_types import Enum as TraitEnum
 from traits.trait_types import Instance as _Instance
 
+P = ParamSpec("P")
+T = TypeVar("T")
+
 OptionalInstance = _Instance  # type: ignore
 
-StrInstance = _Instance  # type: ignore
+
+class StrInstance(_Instance, Generic[T]):  # type: ignore
+    ...
 
 
 class Instance(_Instance):  # type: ignore
@@ -23,12 +28,14 @@ class TraitCallable(_Callable):  # type: ignore
         super().__init__(*args, allow_none=False, **metadata)
 
 
+class TypedCallable(_Callable, Generic[P, T]):  # type: ignore
+    ...
+
+
 if TYPE_CHECKING:
-    from typing import Any, Callable, TypeVar
+    from typing import Any, Callable
 
     from traits.trait_type import _TraitType
-
-    T = TypeVar("T")
 
     class OptionalInstance(_TraitType[T | None, T | None]):
         def __init__(
@@ -39,7 +46,7 @@ if TYPE_CHECKING:
         ):
             ...
 
-    class StrInstance(_TraitType[Any, Any]):
+    class StrInstance(_TraitType[T, T], Generic[T]):
         def __init__(
             self,
             klass: str,
@@ -58,4 +65,7 @@ if TYPE_CHECKING:
             ...
 
     class TraitCallable(_TraitType[Callable, Callable]):
+        ...
+
+    class TypedCallable(_TraitType[Callable[P, T], Callable[P, T]], Generic[P, T]):
         ...
